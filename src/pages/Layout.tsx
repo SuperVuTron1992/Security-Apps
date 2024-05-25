645ce0346cf4ea118eebb74e09488260c08fb6b6
SuperVuTron1992 <tomfoolery1992super@gmail.com>
2024-05-25 02:59:59 -0500

    create update and new stuff

---
+++ src/pages/Layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+interface LayoutProps {}
+
+const Layout: React.FC<LayoutProps> = () => {
+  return <div></div>;
+};
+
+export default Layout;
