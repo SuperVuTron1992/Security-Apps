bd60f702bfdf2b8d31f00a170de7c1ed8dce1a9e
SuperVuTron1992 <tomfoolery1992super@gmail.com>
2024-05-26 15:52:47 -0500

    update the new page

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import HomePage from './HomePage';
 import LoginPage from './LoginPage';
 import Layout from '../components/Layout';
 import './index.css';
-import { Button } from '@material-tailwind/react';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="LoginPage" />} />
           <Route path="LoginPage" element={<LoginPage />} />
           <Route path="HomePage" element={<HomePage />} />
         </Route>
