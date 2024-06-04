5f61911eb9b2b294d27999b617ee4e2bce6ed383
SuperVuTron1992 <tomfoolery1992super@gmail.com>
2024-06-04 00:39:48 -0500

    create the new stuff

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,33 +7,7 @@ import { MuiFileInput } from 'mui-file-input';
 
 const HomePage: React.FC = () => {
   const theme = useTheme();
-  const [UserEmail, setUserEmail] = useState<String | undefined>();
-  const [PassWord, setPassWord] = useState<String | undefined>();
   const [file, setFile] = useState<any>();
-
-  let navigate = useNavigate();
-
-  // check the input that the user input information via check submit button
-  const checkSubmiteButton = async () => {
-    try {
-      const getAuth = await AuthTokenService.AuthToken(PassWord);
-      navigate(`/${getAuth.jwt}/NewStockPage/`);
-    } catch (error) {
-      console.error('Error fetching authentication data:', error);
-    }
-  };
-
-  // listen to the enter button was enter
-  const handleKeyPress = (event: any) => {
-    if (event.key === 'Enter') {
-      checkSubmiteButton();
-    }
-  };
-
-  // check the forgot password button and link to a new forget password page
-  const checkForgotPasswordButton = () => {
-    navigate('/ForgotPassWordPage');
-  };
 
   const handleChange = (newFile: any) => {
     setFile(newFile);
@@ -41,61 +15,35 @@ const HomePage: React.FC = () => {
 
   const checkSubmitfileButtonPress = async () => {
     try {
-      const getAuth = await AuthTokenService.AuthToken(PassWord);
-      navigate(`/${getAuth.jwt}/NewStockPage/`);
+      const formData = new FormData();
+      formData.append('file', file);
+
+      // const getAuth = await AuthTokenService.AuthToken(PassWord);
+      const getFile = await fetch(
+        `https://fluxdux.com/importJsonData/Stock/Properties/testingStttttufff`,
+        {
+          method: 'POST',
+          body: formData,
+        },
+      );
+
+      if (getFile.ok) {
+        const data = await getFile.json();
+        alert('upload the File');
+      }
     } catch (error) {
       console.error('Error fetching authentication data:', error);
     }
   };
 
   const handleSubmitPress = () => {
-    if (File) {
-      // checkSubmitfileButtonPress();
-      console.log('testing stuff');
+    if (file) {
+      checkSubmitfileButtonPress();
     }
   };
 
   return (
     <div className="grid h-screen place-content-center">
-      <div className=" text-3xl font-bold text-blue-600  ml-20 pb-8">
-        <h1> Login Page</h1>
-      </div>
-
-      <div className="mb-8">
-        <input
-          className=" h-16 w-80 border-solid border-2 border-black "
-          placeholder="Email"
-          onChange={(event) => setUserEmail(event.target.value)}
-        />
-      </div>
-
-      <div className="mb-8">
-        <input
-          className=" h-16 w-80 border-solid border-2 border-black"
-          placeholder="Password"
-          onChange={(event) => setPassWord(event.target.value)}
-          onKeyDown={handleKeyPress}
-        />
-      </div>
-
-      <div className="flex w-max gap-4 ">
-        <Button
-          variant="contained"
-          style={{ backgroundColor: theme.palette.green.main, color: 'white' }}
-          onClick={checkSubmiteButton}
-        >
-          {' '}
-          Submit
-        </Button>
-        <Button
-          variant="outlined"
-          style={{ backgroundColor: 'white', color: 'red' }}
-          onClick={checkForgotPasswordButton}
-        >
-          Forgot Password
-        </Button>
-      </div>
-
       <MuiFileInput
         value={file}
         onChange={handleChange}
