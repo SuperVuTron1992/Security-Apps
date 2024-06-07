73462760805e921dfa42963ea7cda557608c0945
SuperVuTron1992 <tomfoolery1992super@gmail.com>
2024-06-07 00:36:42 -0500

    just update

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,42 +4,68 @@ import AuthTokenService from '../services/AuthTokenService';
 import { Button } from '@material-ui/core';
 import { useTheme } from '@mui/material/styles';
 import { MuiFileInput } from 'mui-file-input';
+import axios from 'axios';
 
 const HomePage: React.FC = () => {
   const theme = useTheme();
   const [file, setFile] = useState<any>();
+  const [jsonData, setJsonData] = useState<any>();
 
   const handleChange = (newFile: any) => {
     setFile(newFile);
+    readFile(newFile);
   };
 
-  const checkSubmitfileButtonPress = async () => {
+  const readFile = (file: File) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const text = event.target?.result as string;
+      console.log(JSON.parse(text));
+      setJsonData(JSON.parse(text));
+    };
+  };
+
+  const checkSubmitfileButtonPress = async (data: any) => {
     try {
-      const formData = new FormData();
-      formData.append('file', file);
+      // const formData = new FormData();
+      // formData.append('file', file);
 
       // const getAuth = await AuthTokenService.AuthToken(PassWord);
       const getFile = await fetch(
         `https://fluxdux.com/importJsonData/Stock/Properties/testingStttttufff`,
         {
           method: 'POST',
-          body: formData,
+          body: data,
         },
       );
 
       if (getFile.ok) {
-        const data = await getFile.json();
-        alert('upload the File');
+        const dataPull = await getFile.json();
+        console.log('upload the File', dataPull);
       }
     } catch (error) {
       console.error('Error fetching authentication data:', error);
     }
   };
 
+  // const checkSubmitfileButtonPress = async (data: any) => {
+  //   try {
+  //     const response = await axios.post(
+  //       `https://fluxdux.com/importJsonData/Stock/Properties/testingStttttufff`,
+  //       data,
+  //     );
+
+  //     if (response.status === 200) {
+  //       alert('File data uploaded successfully');
+  //     }
+  //   } catch (error) {
+  //     console.error('Error uploading file data:', error);
+  //   }
+  // };
+
   const handleSubmitPress = () => {
-    if (file) {
-      checkSubmitfileButtonPress();
-    }
+    console.log('hasclick');
+    checkSubmitfileButtonPress(jsonData);
   };
 
   return (
